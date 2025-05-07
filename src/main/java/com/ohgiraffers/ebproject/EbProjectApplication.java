5ebaccc2a8997fad9e0a4d9a759cce208e3e0819
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-05-07 16:20:42 +0900

    first

---
+++ src/main/java/com/ohgiraffers/ebproject/EbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.ebproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbProjectApplication.class, args);
+    }
+
+}
