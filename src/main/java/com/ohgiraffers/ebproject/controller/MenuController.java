07cb62dea62afe9ea98e57d9c19d00541fa413b5
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-05-07 17:20:06 +0900

    second pr

--- src/main/java/com/ohgiraffers/ebproject/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/ebproject/controller/MenuController.java
@@ -19,7 +19,7 @@ public class MenuController {
 
     @GetMapping("health")
     public String healthCheck() {
-        return "eb_project is online and healthy! dev/feature/test 테스트";
+        return "eb_project is online and healthy!";
     }
 
     @GetMapping("menus/{menuCode}")
