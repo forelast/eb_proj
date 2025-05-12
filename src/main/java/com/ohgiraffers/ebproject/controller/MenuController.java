250df2313a62f5bf575568a860ebfcad2b9c3153
wolfohgiraffers <wolf.ohgiraffers@gmail.com>
2025-05-12 17:02:58 +0900

    blue change

--- src/main/java/com/ohgiraffers/ebproject/controller/MenuController.java
+++ src/main/java/com/ohgiraffers/ebproject/controller/MenuController.java
@@ -12,7 +12,7 @@ public class MenuController {
 
     private final MenuService menuService;
 
-    private static final String HEALTH_MESSAGE = "eb_pro is online and green!";
+    private static final String HEALTH_MESSAGE = "eb_pro is online and blue2!";
 
     @Autowired
     public MenuController(MenuService menuService) {
